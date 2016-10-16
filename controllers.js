c787fab43f79019ecd46408317416128172745e8
ouyangdanlin <oydl911@hotmail.com>
2016-10-16 16:05:12 +0300

    test

--- controllers.js
+++ controllers.js
@@ -3,7 +3,7 @@
  */
 argular.mobile('starter.controllers',[])
 
-.controller('signCtrl',function($scape)){
+.controller('signCtrl',function($scope)){
     $scape.user = {
         name:
     };
@@ -14,4 +14,22 @@ argular.mobile('starter.controllers',[])
     }
 })
 
-.controller('feedCtrl',function($scape))
+.controller('feedCtrl',function($scope){
+
+    $http.git(https://dashboard.heroku.com/apps/emailmessage).then(function(response){
+        console.log(response);
+    $scope.data = response.data;
+    });
+
+$scope.send = function()
+{
+    var datawith Post = {
+        username: "Example",
+        message:"something"
+};
+    $http.post(https://dashboard.heroku.com/apps/emailmessage).datawithPost.then(function(response){
+    console.log(response);
+    $scope.data = response.data;
+});
+}
+})
